6a384738ed89c1966898dc853740dfc600dd5166
danutek <36863617+danutek@users.noreply.github.com>
2024-10-29 13:56:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-# main.tf (in the root directory)
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "s3_bucket" {
-  source      = "./modules/s3_bucket"    # Path to the module directory
-  bucket_name = "my-unique-bucket-name"  # Set the bucket name
-  bucket_acl  = "public-read"            # Set the bucket ACL (optional, overrides the default)
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = var.bucket_acl
 }
